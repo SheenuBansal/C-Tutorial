9d6b7ca7efe528aa3330defde7845c6a9c03b3e2
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-10 01:32:23 +0530

    Taking user input

---
+++ 3input.c
@@ -0,0 +1,18 @@
+# include<stdio.h>
+
+int main(){
+
+    int a,b ;
+    printf("Printing sum of two integers by taking user input\n");
+
+    printf("Enter integer value of a:\n");
+    scanf("%d",&a);
+
+    printf("Enter integer value of b:\n");
+    scanf("%d",&b);
+
+    printf("Sum of a and b is %d \n",a+b);
+
+    return 0;
+    
+}
