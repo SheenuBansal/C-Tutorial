9abbe3b68003753a7ce35eeddb8f9288e64b7bfb
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-10 01:03:35 +0530

    Varibles in C

--- 2.variables.c
+++ 2.variables.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int a = 4;
+    int a = 42;
     int b = 7.8; // Not recommended
     float c = 9.2;
     char d = 'M';
@@ -11,7 +11,7 @@ int main()
     printf("\nThe value of float c is %f", c);
     printf("\nThe value of char d is %c", d);
     printf("\nThe value of sum of a and b is %f", a + b); // Not correct integer and float sum
-    printf("\nThe value of sum of a and e is %f", a + e);
+    printf("\nThe value of sum of a and e is %d", a + e);
 
     return 0;
 }
