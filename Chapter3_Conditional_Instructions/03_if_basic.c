5baba36ca3888efcbc138ac8f1d6b48a9c54e79a
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-10 20:08:40 +0530

    Checking even or odd number

---
+++ Chapter3_Conditional_Instructions/03_if_basic.c
@@ -0,0 +1,20 @@
+// Program to Check if given number is even or odd
+
+# include<stdio.h>
+
+int main()
+{
+    int a ;
+    printf("Enter any number: \n");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("%d is even",a);
+    }
+    
+    else
+    {
+        printf("%d is odd",a);
+    }
+    return 0;
+}
---
+++ Chapter3_Conditional_Instructions/03_if_else_2.c
@@ -0,0 +1,32 @@
+// "=" in C is assignment operator , whereas "==" is quality check operator.
+
+// Look at below program 
+
+# include<stdio.h>
+
+int main() 
+{
+    int age ;
+    printf("Enter your age: \n");
+    scanf("%d",&age);
+    if (age%2==0)
+    {
+        printf("%d is even\n",age);
+    }
+    
+    else
+    {
+        printf("%d is odd\n",age);
+    }
+
+    // Following block of code is executed for whatever vaue of age, 
+    // because it is assigning value to age , not doing check.
+    // so BE CAREFUL
+    
+    if (age=50)
+    {
+        printf("half century");
+    }
+
+    return 0;
+}
