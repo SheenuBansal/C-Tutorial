40700b855ca67c2d95a96ce403a826979b1c50a8
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-10 17:17:17 +0530

    Arithmetic Errors discussion

--- Chapter2_Instructions/02_arithmetic_instructions.c
+++ Chapter2_Instructions/02_arithmetic_instructions.c
@@ -1,11 +1,14 @@
 # include<stdio.h>
+# include<math.h>
 
 int main()
 {
     int a=3;
-    int b=4;
+    int b=8;
     int z= b*a;
     // b*a = z; // Illegal will throw error
-    printf("Value of z is: %d",z);
+    printf("Value of z is: %d\n",z);
+    printf("value of a to the power b is %f",pow(a,b));
     return 0;
+
 }
