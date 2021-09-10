890f38799b4ad67c546058476b518550efd8d309
Sheenu Bansal <sheenubansal308@gmail.com>
2021-09-10 15:49:20 +0530

    Practice Program: Calculate Area with given values

---
+++ 01_practice.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Calculating Area of Circle by hard coding value of radius.
+
+int main()
+{
+    int r = 4;
+    printf("Area of circle is: %f", 3.14 * r * r);
+    return 0;
+}
+
+// Another way is :
+
+// int main()
+// {
+
+//     int r = 4;
+//     float area = 3.12 * r * r;
+//     printf("Area of circle is: %f", area);
+
+//     return 0;
+// }
